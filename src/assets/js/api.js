2a86b9c516f949c6983bd4ef0ecaba9cbaa2dd4d
Apple <riyabulia12@gmail.com>
2022-08-24 17:57:49 +0530

    Fetch data from api

---
+++ src/assets/js/api.js
@@ -0,0 +1,16 @@
+const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/90yo8SECzswGWAW8W1Yn/scores';
+
+export const getScores = async () => {
+   const response = await fetch(baseUrl).then((response) => response.json());
+   return response.result;
+};
+
+export const postScore = async (bodyData) => {
+   await fetch(baseUrl, {
+      method: 'POST',
+      body: JSON.stringify(bodyData),
+      headers: {
+         'Content-type': 'application/json; charset=UTF-8',
+      },
+   });
+};
